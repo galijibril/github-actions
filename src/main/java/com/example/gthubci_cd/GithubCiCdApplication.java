5849980f6650d1766b9b33990bdddc5aff1496e5
Gali Jibril <galijibril001@gmail.com>
2022-01-21 15:24:51 +0100

    code

---
+++ src/main/java/com/example/gthubci_cd/GithubCiCdApplication.java
@@ -0,0 +1,28 @@
+package com.example.gthubci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCiCdApplication {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome to java";
+    }
+
+//    echo "# github-actions" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/galijibril/github-actions.git
+//    git push -u origin main
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCiCdApplication.class, args);
+    }
+
+}
